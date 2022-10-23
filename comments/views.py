2ead3ea4d0f31225682aefbb980bf88b73e6a712
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2022-10-23 18:08:39 +0900

    flash message

--- comments/views.py
+++ comments/views.py
@@ -6,6 +6,8 @@ from django.views.generic import DetailView
 from django.views.generic import CreateView
 from django.views.generic import UpdateView
 from django.views.generic import DeleteView
+from django.shortcuts import redirect
+from django.contrib import messages
 from .forms import CommentForm
 from .models import Comment
 
@@ -33,6 +35,10 @@ class CreateCommentView(CreateView):
         context['form_name'] = 'コメントの投稿'
         context['button_label'] = 'コメントを投稿する'
         return context
+    def form_valid(self, form):
+        self.object = comment = form.save()
+        messages.success(self.request, 'コメントを投稿しました')
+        return redirect(self.get_success_url())
 
 class UpdateCommentView(UpdateView):
     model = Comment
