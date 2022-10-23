8ea37c63319ea1e6cf5ed9c8b73eb2e3429e5173
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2022-10-23 17:22:20 +0900

    config ja

--- django_comment_mysql/settings.py
+++ django_comment_mysql/settings.py
@@ -97,13 +97,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ja"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Tokyo"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
