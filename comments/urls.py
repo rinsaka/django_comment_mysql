4fc85b8cc598dc727ecb3cd758cc2b5c26ce3b84
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2022-10-23 17:58:47 +0900

    update

--- comments/urls.py
+++ comments/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.CommentIndexView.as_view(), name='index'),
     path('<int:pk>/', views.ShowCommentView.as_view(), name='show'),
     path('create/', views.CreateCommentView.as_view(), name='create'),
+    path('<int:pk>/update/', views.UpdateCommentView.as_view(), name='update'),
 ]
--- comments/views.py
+++ comments/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic import DetailView
 from django.views.generic import CreateView
+from django.views.generic import UpdateView
 from .forms import CommentForm
 from .models import Comment
 
@@ -19,6 +20,11 @@ class ShowCommentView(DetailView):
     model = Comment
 
 class CreateCommentView(CreateView):
+    model = Comment
+    form_class = CommentForm
+    success_url = reverse_lazy('comments:index')
+
+class UpdateCommentView(UpdateView):
     model = Comment
     form_class = CommentForm
     success_url = reverse_lazy('comments:index')
